9de38ddd08707ab066d3410f29d06aea34ec6e74
郑震 <2693781894@qq.com>
2024-01-14 14:37:52 +0800

    exercise 1 ex2:use object to exercise useReducer

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,23 +3,24 @@
 
 import * as React from 'react'
 
-function reducer(oldVal,cnt){
+function reducer(oldVal, {cnt,name}){
   console.log(`oldVal:${oldVal}`)
   console.log(`cnt:${cnt}`)
-  return oldVal+cnt
+  return {cnt:oldVal.cnt+cnt,name:oldVal.name+name}
 }
 function Counter({initialCount = 0, step = 1}) {
   // 🐨 replace React.useState with React.useReducer.
   // 💰 React.useReducer(countReducer, initialCount)
-  const [cnt,SetCnt] = React.useReducer(reducer,initialCount)
+  const [state,SetState] = React.useReducer(reducer, {cnt: initialCount,name: initialCount.toString()})
   //const [count, setCount] = React.useState(initialCount)
+  const {cnt, name} = state
 
   // 💰 you can write the countReducer function so you don't have to make any
   // changes to the next two lines of code! Remember:
   // The 1st argument is called "state" - the current value of count
   // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => SetCnt(step)
-  return <button onClick={increment}>{cnt}</button>
+  const increment = () => SetState({cnt:step,name:step.toString()})
+  return <button onClick={increment}>{`${name}--${cnt}`}</button>
 }
 
 function App() {
