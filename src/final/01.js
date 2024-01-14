9b8c75ed7eb2c0e60c8b81ec7052194ac23081c6
郑震 <2693781894@qq.com>
2024-01-14 15:55:39 +0800

    exercise :practice useMemo

--- src/final/01.js
+++ src/final/01.js
@@ -8,10 +8,39 @@ const countReducer = (state, newState) => newState
 function Counter({initialCount = 0, step = 1}) {
   const [count, setCount] = React.useReducer(countReducer, initialCount)
   const increment = () => setCount(count + step)
-  return <button onClick={increment}>{count}</button>
+  const myList = React.useMemo(()=>{
+       return (
+         <MyList cnt={count}/>
+       )
+    }
+  ,[count])
+
+  return (
+    <>
+    <button onClick={increment}>{count}</button><Test/>
+      {myList}
+    </>
+  )
+}
+// function Test(){
+//   console.log("static is rendered")
+//   return <div>i am static</div>
+// }
+function MyList({cnt}){
+  console.log("list is rendered")
+  const list = []
+  for (let i=0;i<cnt;i++){
+    list.push(<div>item{i}</div>)
+  }
+  return (
+    <>
+      {list}
+    </>
+  )
 }
 
 function App() {
+
   return <Counter />
 }
 
